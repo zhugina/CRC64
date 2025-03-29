7f3a8c89d6972baac1e5ba72b7824dc71f37ef89
zhugina <zhuginaes@gmail.com>
2025-03-29 16:55:16 +0300

    added crc64 algorithm

--- include/crc64.h
+++ include/crc64.h
@@ -2,7 +2,7 @@
 #define CRC64
 
 #include <stdint.h>
-#define ECMA_182 = 0x42F0E1EBA9EA3693
+#define ECMA_182 0x42F0E1EBA9EA3693
 
 uint64_t crc64(const uint8_t *in, size_t in_length);
 
--- src/crc64.c
+++ src/crc64.c
@@ -1,6 +1,18 @@
 #include "crc64.h"
 
-uint64_t crc64(const uint8_t *in, size_t in_length)
+uint64_t crc64(const uint8_t *data, size_t len)
 {
-    return 0;
+    uint64_t crc = 0;
+    for (size_t i = 0; i < len; i++)
+    {
+        crc ^= (uint64_t)data[i] << 56;
+        for (int j = 0; j < 8; j++)
+        {
+            if (crc & (1ULL << 63))
+                crc = (crc << 1) ^ ECMA_182;
+            else
+                crc <<= 1;
+        }
+    }
+    return crc;
 }
